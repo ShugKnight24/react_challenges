d4da7e834c62e5ac4d1cd2e332534c911b002e46
Shugmi Shumunov <SShumunov@gmail.com>
2022-02-21 16:20:01 -0500

    feat: Add RandomUserCards that displays data to the UI

--- src/Components/Pages/RandomUser.tsx
+++ src/Components/Pages/RandomUser.tsx
@@ -2,15 +2,28 @@
 // will contain random user cards
 import { FC } from 'react';
 import { RandomUserResponse } from '../../Types/ApiResponses/RandomUser';
-
+import { RandomUserCard } from '../RandomUser';
 interface RandomUserProps {
 	userData: RandomUserResponse | null;
 }
 
 export const RandomUser: FC<RandomUserProps> = (props) => {
 	
+	const {
+		userData
+	} = props;
+
+	const userDataArray = userData?.results;
 	return(
 		<div className="random-user-container">
+			{userDataArray?.map((user) => {
+				return (
+					<RandomUserCard
+						key={user?.id?.value}
+						userData={user}
+					/>
+				);
+			})}
 		</div>
 	);
 };
---
+++ src/Components/RandomUser/index.ts
@@ -0,0 +1,1 @@
+export { RandomUserCard } from './Card';
